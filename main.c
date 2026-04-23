0347e84b9dff9b4c1a1313239f28513e4375cf10
god <alexandimis@gmail.com>
2026-04-23 20:45:18 +0300

    Directory setup with clion

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbit.h>
+
+int main() {
+    /* INITIALIZATIONS*/
+    bool running = true;
+
+    /* MAIN LOOP */
+    while (running) {
+        printf("Hello World!\n");
+        running = false;
+    }
+
+    return 0;
+}
